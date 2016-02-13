67daed805e8f0dc3c4a7a765186b643e7b3bab8b
Connor Patterson <tcpatter@ncsu.edu>
2016-02-13 16:09:30 -0500

    add hibernate dependency and attempt a dao example

--- src/main/java/com/seniordesigndbgt/dashboard/dao/UserDAO.java
+++ src/main/java/com/seniordesigndbgt/dashboard/dao/UserDAO.java
@@ -1,4 +1,23 @@
 package com.seniordesigndbgt.dashboard.dao;
 
+import com.seniordesigndbgt.dashboard.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
 public class UserDAO {
+
+    private SessionFactory sessionFactory;
+
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public void save(User u) {
+        Session session = this.sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+        session.persist(u);
+        tx.commit();
+        session.close();
+    }
 }
