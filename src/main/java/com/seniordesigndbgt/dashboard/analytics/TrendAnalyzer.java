bd36d6ace32a142315b8737dc60cc39dd1658848
Neel Kuila <neelk1123@gmail.com>
2016-02-22 15:24:49 -0500

    Gets word map from text

--- src/main/java/com/seniordesigndbgt/dashboard/analytics/TrendAnalyzer.java
+++ src/main/java/com/seniordesigndbgt/dashboard/analytics/TrendAnalyzer.java
@@ -1,8 +1,15 @@
 package com.seniordesigndbgt.dashboard.analytics;
 
+import com.seniordesigndbgt.dashboard.model.Press;
+
+import java.text.BreakIterator;
+import java.util.HashMap;
+import java.util.Map;
+
 public class TrendAnalyzer {
 
     public String algorithm = "x mentions in y time";
+    public TrendAnalyzer() {}
 
     public void analyzeCurrentTrends() {
         //TODO
@@ -11,4 +18,28 @@ public class TrendAnalyzer {
     public void findNewTrends() {
         //TODO
     }
+
+    public Map<String, Integer> getWordMap(String text){
+        Map<String, Integer> hashMap = new HashMap<String, Integer>();
+        String removeCharacters = ".,!?"; //Remove these characters from the text
+        for (int i = 0; i < removeCharacters.length(); i++){
+            text = text.replace(String.valueOf(removeCharacters.charAt(i)), "");
+        }
+
+        String[] splitArray = text.split(" ");
+
+
+        for (String currWord : splitArray) {
+//            System.out.println(currWord);
+            if (hashMap.containsKey(currWord)) {
+                int currCount = hashMap.get(currWord);
+                hashMap.put(currWord, currCount + 1);
+            }
+            else {
+                hashMap.put(currWord, 1);
+            }
+        }
+        return hashMap;
+    }
+
 }
--- src/main/java/com/seniordesigndbgt/dashboard/scheduler/TrendSchedule.java
+++ src/main/java/com/seniordesigndbgt/dashboard/scheduler/TrendSchedule.java
@@ -1,7 +1,27 @@
 package com.seniordesigndbgt.dashboard.scheduler;
 
-/**
- * Created by thomaspatterson on 2/14/16.
- */
+import com.seniordesigndbgt.dashboard.analytics.TrendAnalyzer;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Iterator;
+import java.util.Map;
+
+@Component
 public class TrendSchedule {
+
+    TrendAnalyzer testAnalyzer = new TrendAnalyzer();
+    @Scheduled(fixedDelay = 10000)
+    public void testStringBreak(){
+        String testSource = "This should split up into this, that, and the other other other other this this up up.";
+        System.out.println(testSource);
+        Map<String, Integer> map = testAnalyzer.getWordMap(testSource);
+
+        Iterator it = map.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry)it.next();
+            System.out.println(pair.getKey() + " = " + pair.getValue());
+            it.remove(); // avoids a ConcurrentModificationException
+        }
+    }
 }
