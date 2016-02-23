65b615c256ee85cf7c879847d2ba446265cb8896
Neel Kuila <neelk1123@gmail.com>
2016-02-23 12:44:01 -0500

    Added sort method

--- src/main/java/com/seniordesigndbgt/dashboard/analytics/TrendAnalyzer.java
+++ src/main/java/com/seniordesigndbgt/dashboard/analytics/TrendAnalyzer.java
@@ -3,24 +3,45 @@ package com.seniordesigndbgt.dashboard.analytics;
 import com.seniordesigndbgt.dashboard.model.Press;
 
 import java.text.BreakIterator;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class TrendAnalyzer {
 
-    public String algorithm = "x mentions in y time";
+    private String algorithm = "x mentions in y time";
+    private Map<String, Integer> longFrequencyMap;
+    private Map<String, Integer> shortFrequencyMap;
+
     public TrendAnalyzer() {}
 
+    private static TrendAnalyzer singleton;
+
+    public static TrendAnalyzer getInstance(){
+        if (singleton == null)
+            singleton = new TrendAnalyzer();
+        return singleton;
+    }
+    //TODO Need to separate already analyzed from text that needs analysis
     public void analyzeCurrentTrends() {
         //TODO
     }
 
     public void findNewTrends() {
         //TODO
+        //Sort the new hashMap
+        //Check for threshold
+        //
     }
 
-    public Map<String, Integer> getWordMap(String text){
-        Map<String, Integer> hashMap = new HashMap<String, Integer>();
+    public void refreshLongMap(){
+        longFrequencyMap = new LinkedHashMap<String, Integer>();
+    }
+
+    public void refreshShortMap(){
+        shortFrequencyMap = new LinkedHashMap<String, Integer>();
+    }
+
+    public void updateFrequencyMap(String text){
+        //Sanitize input
         String removeCharacters = ".,!?"; //Remove these characters from the text
         for (int i = 0; i < removeCharacters.length(); i++){
             text = text.replace(String.valueOf(removeCharacters.charAt(i)), "");
@@ -31,15 +52,34 @@ public class TrendAnalyzer {
 
         for (String currWord : splitArray) {
 //            System.out.println(currWord);
-            if (hashMap.containsKey(currWord)) {
-                int currCount = hashMap.get(currWord);
-                hashMap.put(currWord, currCount + 1);
+            if (longFrequencyMap.containsKey(currWord)) {
+                int currCount = longFrequencyMap.get(currWord);
+                longFrequencyMap.put(currWord, currCount + 1);
+            } else {
+                longFrequencyMap.put(currWord, 1);
             }
-            else {
-                hashMap.put(currWord, 1);
+
+            if (shortFrequencyMap.containsKey(currWord)) {
+                int currCount = shortFrequencyMap.get(currWord);
+                shortFrequencyMap.put(currWord, currCount + 1);
+            } else {
+                shortFrequencyMap.put(currWord, 1);
             }
         }
-        return hashMap;
+    }
+
+    public static <K, V extends Comparable<? super V>> Map<K, V> sortFrequencyMapByValue(Map<K,V> map) {
+        List<Map.Entry<K, V>> list = new LinkedList<Map.Entry<K, V>>(map.entrySet());
+        Collections.sort(list, new Comparator<Map.Entry<K, V>>() {
+            public int compare(Map.Entry<K, V> o1, Map.Entry<K, V> o2) {
+                return (o1.getValue()).compareTo(o2.getValue());
+            }
+        });
+        Map<K, V> result = new LinkedHashMap<K, V>();
+        for (Map.Entry<K, V> entry : list) {
+            result.put(entry.getKey(), entry.getValue());
+        }
+        return result;
     }
 
 }
--- src/main/java/com/seniordesigndbgt/dashboard/scheduler/TrendSchedule.java
+++ src/main/java/com/seniordesigndbgt/dashboard/scheduler/TrendSchedule.java
@@ -4,6 +4,7 @@ import com.seniordesigndbgt.dashboard.analytics.TrendAnalyzer;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -14,8 +15,9 @@ public class TrendSchedule {
     @Scheduled(fixedDelay = 10000)
     public void testStringBreak(){
         String testSource = "This should split up into this, that, and the other other other other this this up up.";
-        System.out.println(testSource);
-        Map<String, Integer> map = testAnalyzer.getWordMap(testSource);
+//        System.out.println(testSource);
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        testAnalyzer.getWordMap(testSource, map);
 
         Iterator it = map.entrySet().iterator();
         while (it.hasNext()) {
