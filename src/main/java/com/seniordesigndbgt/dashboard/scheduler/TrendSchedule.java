fc229a1011fb38e89eb317f0742686e8eacb5811
Connor <tcpatter@ncsu.edu>
2016-02-24 10:01:03 -0500

    Revert "Gets top three words over threshold of text"

--- src/main/java/com/seniordesigndbgt/dashboard/scheduler/TrendSchedule.java
+++ src/main/java/com/seniordesigndbgt/dashboard/scheduler/TrendSchedule.java
@@ -1,25 +1,7 @@
 package com.seniordesigndbgt.dashboard.scheduler;
 
-import com.seniordesigndbgt.dashboard.analytics.TrendAnalyzer;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-
-@Component
+/**
+ * Created by thomaspatterson on 2/14/16.
+ */
 public class TrendSchedule {
-
-    TrendAnalyzer testAnalyzer = new TrendAnalyzer();
-    @Scheduled(fixedDelay = 10000)
-    public void testStringBreak(){
-        String testSource = "This should split up into this, that, and the other other other other this this up up.";
-//        System.out.println(testSource);
-        testAnalyzer.refreshShortMap();
-        testAnalyzer.refreshLongMap();
-        testAnalyzer.updateFrequencyMap(testSource);
-        testAnalyzer.findNewTrends();
-
-    }
 }
