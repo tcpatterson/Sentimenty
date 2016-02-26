a442cd3b6267114bddad8066f51f59245ffa7ef4
Owen Hammer <ojhammer@ncsu.edu>
2016-02-26 00:20:17 -0500

    finalized the testing for the sentiment analyzer with factory options

--- src/test/com/seniordesigndbgt/dashboard/analytics/SentimentAnalyzerTest.java
+++ src/test/com/seniordesigndbgt/dashboard/analytics/SentimentAnalyzerTest.java
@@ -4,6 +4,7 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonPrimitive;
+import com.seniordesigndbgt.dashboard.model.Press;
 import junit.framework.TestCase;
 import org.aspectj.runtime.reflect.Factory;
 import org.junit.After;
@@ -27,10 +28,11 @@ public class SentimentAnalyzerTest extends TestCase{
 
     }
 
+    //testing for strings
     @Test
-    public void testGetSentiment() throws Exception {
-        String testNeg = "I fucking hate this.";
-        String testPos = "This is so awesome!";
+    public void testGetSentimentText() throws Exception {
+        String testNeg = "I hate potatoes. They are the worst. Not even ketchup will save them";
+        String testPos = "This is so awesome! I love tater tots. They are so amazing with honey mustard";
         String negRes = analyzer.getSentiment(testNeg);
         String posRes = analyzer.getSentiment(testPos);
         JsonElement jElementNeg = new JsonParser().parse(negRes);
@@ -38,15 +40,29 @@ public class SentimentAnalyzerTest extends TestCase{
         JsonObject jObject = jElementNeg.getAsJsonObject();
         JsonPrimitive jPrimitive = jObject.getAsJsonPrimitive("score");
         Double n = jPrimitive.getAsDouble();
-        assertTrue("n between -1 and 0", n<0.00 && n>-1.00);
+        assertTrue("n between -1 and 0", n < 0.00 && n > -1.00);
         jObject = jElementPos.getAsJsonObject();
         jPrimitive = jObject.getAsJsonPrimitive("score");
         n = jPrimitive.getAsDouble();
-        assertTrue("n between -1 and 1", n<0.00 && n>1.00);
+        assertTrue("n between -1 and 1", n > 0.00 && n < 1.00);
     }
 
+    //testing for articles
     @Test
-    public void testGetSentiment1() throws Exception {
-
+    public void testGetSentimentArticle() throws Exception {
+        Press badArticle = new Press("badArticle", "http://www.bloomberg.com/news/articles/2016-02-25/ex-deutsche-bank-trader-zhou-admitted-to-mismarking-cmbs-trades", "The Big Bad Wolf");
+        Press goodArticle = new Press("goodArticle", "http://www.bloomberg.com/news/audio/2016-02-23/silverstein-balance-sheets-are-best-drivers-of-performance-now", "Fluffy Bunnies");
+        String negRes = analyzer.getSentiment(badArticle);
+        String posRes = analyzer.getSentiment(goodArticle);
+        JsonElement jElementNeg = new JsonParser().parse(negRes);
+        JsonElement jElementPos = new JsonParser().parse(posRes);
+        JsonObject jObject = jElementNeg.getAsJsonObject();
+        JsonPrimitive jPrimitive = jObject.getAsJsonPrimitive("score");
+        Double n = jPrimitive.getAsDouble();
+        assertTrue("n between -1 and 0", n < 0.00 && n > -1.00);
+        jObject = jElementPos.getAsJsonObject();
+        jPrimitive = jObject.getAsJsonPrimitive("score");
+        n = jPrimitive.getAsDouble();
+        assertTrue("n between -1 and 1", n > 0.00 && n < 1.00);
     }
 }
