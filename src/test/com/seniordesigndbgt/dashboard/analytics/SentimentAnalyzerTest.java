c756ed25c9ed19e2b8d04f2554636c9648b7debe
Owen Hammer <ojhammer@ncsu.edu>
2016-02-25 22:54:09 -0500

    working junit tests in gradle now

--- src/test/com/seniordesigndbgt/dashboard/analytics/SentimentAnalyzerTest.java
+++ src/test/com/seniordesigndbgt/dashboard/analytics/SentimentAnalyzerTest.java
@@ -3,17 +3,23 @@ package com.seniordesigndbgt.dashboard.analytics;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.gson.JsonPrimitive;
 import junit.framework.TestCase;
+import org.aspectj.runtime.reflect.Factory;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.omg.CORBA.DoubleHolder;
+import com.seniordesigndbgt.dashboard.analytics.SentimentAnalyzer;
+import com.seniordesigndbgt.dashboard.analytics.AnalyzerFactory;
+import org.gradle.testkit.runner.BuildResult;
+import org.gradle.testkit.runner.GradleRunner;
 
 public class SentimentAnalyzerTest extends TestCase{
     private SentimentAnalyzer analyzer;
     @Before
     public void setUp() throws Exception {
-        analyzer = SentimentAnalyzer.getInstance();
+        analyzer = AnalyzerFactory.getSentimentAnalyzer();
     }
 
     @After
@@ -28,11 +34,12 @@ public class SentimentAnalyzerTest extends TestCase{
         String negRes = analyzer.getSentiment(testNeg);
         String posRes = analyzer.getSentiment(testPos);
         JsonElement jElementNeg = new JsonParser().parse(negRes);
-        JsonElement jElementPos = new JsonParser().parse(negRes);
+        JsonElement jElementPos = new JsonParser().parse(posRes);
         JsonObject jObject = jElementNeg.getAsJsonObject();
-        jObject = jObject.getAsJsonObject("score");
-        Double n = jObject.getAsDouble();
+        JsonPrimitive jsonPrimitive = jObject.getAsJsonPrimitive("score");
+        Double n = jsonPrimitive.getAsDouble();
         assertTrue("n between -1 and 0", n<0.00 && n>-1.00);
+        jObject = jElementPos.getAsJsonObject();
     }
 
     @Test
