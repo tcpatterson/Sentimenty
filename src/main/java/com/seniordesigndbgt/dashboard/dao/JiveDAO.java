5d24b05395630cfb6a6296938d23dab52403675a
Connor Patterson <tcpatter@ncsu.edu>
2016-02-28 16:59:28 -0500

    finish dao and model classes

--- src/main/java/com/seniordesigndbgt/dashboard/dao/JiveDAO.java
+++ src/main/java/com/seniordesigndbgt/dashboard/dao/JiveDAO.java
@@ -1,7 +1,40 @@
 package com.seniordesigndbgt.dashboard.dao;
 
-/**
- * Created by thomaspatterson on 2/14/16.
- */
+import com.seniordesigndbgt.dashboard.model.Jive;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+@Transactional
 public class JiveDAO {
+
+    @Autowired
+    private SessionFactory _sessionFactory;
+
+    private Session getSession() {
+        return _sessionFactory.getCurrentSession();
+    }
+
+    public void save(Jive jive) {
+        getSession().save(jive);
+    }
+
+    public void delete(Jive jive) {
+        getSession().delete(jive);
+    }
+
+    @SuppressWarnings("unchecked")
+    public List getAll() {
+        return getSession().createQuery("from Jive").list();
+    }
+
+    public void update(Jive jive){
+        getSession().update(jive);
+    }
+
 }
--- src/main/java/com/seniordesigndbgt/dashboard/model/Jive.java
+++ src/main/java/com/seniordesigndbgt/dashboard/model/Jive.java
@@ -1,4 +1,72 @@
 package com.seniordesigndbgt.dashboard.model;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+
+@Entity
+@Table(name="jive")
 public class Jive {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
+
+    @NotNull
+    @Size(min = 3, max = 200)
+    @Column(name = "url")
+    private String url;
+
+    @NotNull
+    @Size(min = 3, max = 1000)
+    @Column(name = "content", unique = true)
+    private String content;
+
+    @Size(min = 3, max = 200)
+    @Column(name = "sentiment")
+    private String sentiment;
+
+    public Jive() {}
+
+    public Jive(String url, String content, String sentiment) {
+        this.url = url;
+        this.content = content;
+        this.sentiment = sentiment;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSentiment() {
+        return sentiment;
+    }
+
+    public void setSentiment(String sentiment) {
+        this.sentiment = sentiment;
+    }
+
 }
