3d79a75f7e2a48b057c378d24cc1d62c083f496a
Neel Kuila <neelk1123@gmail.com>
2016-03-20 17:05:59 -0400

    Displaying trends

--- src/main/resources/static/js/trends.js
+++ src/main/resources/static/js/trends.js
@@ -1,37 +1,41 @@
 $( document ).ready(function() {
-//    $.get( "/showPress", function( data ) {
-//      data.forEach(function(d) {
-//        var jsonn = JSON.parse(d.sentiment);
-//        d.sentiment = jsonn;
-//        if(d.sentiment.score){
-//            d.sentiment.score = d.sentiment.score.substring(0,6);
-//        } else {
-//            d.sentiment.score = "0.00";
-//        }
-//        if(d.sentiment.score < 0 ) {
-//            d.arrow = "bottom";
-//            d.color = "red";
-//        } else {
-//            d.arrow = "top";
-//            d.color = "green";
-//        }
-//      });
-//      $("#clientTemplate").tmpl(data).appendTo( "#mentions" );
-//    });
-    var numOfTrendsToDisplay = 4;
+    $.get( "/showPress", function( data ) {
+      data.forEach(function(d) {
+        var jsonn = JSON.parse(d.sentiment);
+        d.sentiment = jsonn;
+        if(d.sentiment.score){
+            d.sentiment.score = d.sentiment.score.substring(0,6);
+        } else {
+            d.sentiment.score = "0.00";
+        }
+        if(d.sentiment.score < 0 ) {
+            d.arrow = "bottom";
+            d.color = "red";
+        } else {
+            d.arrow = "top";
+            d.color = "green";
+        }
+      });
+      $("#clientTemplate").tmpl(data).appendTo( "#mentions" );
+    });
+    var numOfTrendsToDisplay = 5;
     $.get( "/trends", function( data ) {
         for (var i = 0; i < numOfTrendsToDisplay; i++){
-            $(".trend").append("<button>" + data[0][i] + "</button>")
+            $("<button onclick=console.log($(this).text())>" + data[0][i] +
+                "</button>").addClass("trendButton").appendTo(".trend")
         }
     })
 });
 
-$("#reuters").click(function() {
-    $(".mention").addClass("hide");
-    $(".Reuters").removeClass("hide");
+$(".trendButton").click(function(){
+    console.log($(this).text());
 });
-
-$("#bloomberg").click(function() {
-    $(".mention").addClass("hide");
-    $(".Bloomberg").removeClass("hide");
-});
+//$("#reuters").click(function() {
+//    $(".mention").addClass("hide");
+//    $(".Reuters").removeClass("hide");
+//});
+//
+//$("#bloomberg").click(function() {
+//    $(".mention").addClass("hide");
+//    $(".Bloomberg").removeClass("hide");
+//});
