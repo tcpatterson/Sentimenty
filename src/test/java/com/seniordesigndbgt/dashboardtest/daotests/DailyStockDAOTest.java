3e91d506a6fe082dbeff0cea7cda598896fdf758
Kamaria Hardy <kehardy3@ncsu.edu>
2016-03-21 10:23:06 -0400

    created test for DailyStockDAO

---
+++ src/test/java/com/seniordesigndbgt/dashboardtest/daotests/DailyStockDAOTest.java
@@ -0,0 +1,31 @@
+package com.seniordesigndbgt.dashboardtest.daotests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by kamehardy on 3/21/16.
+ */
+public class DailyStockDAOTest {
+
+    @Test
+    public void testSave() throws Exception {
+
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAll() throws Exception {
+
+    }
+
+    @Test
+    public void testClearDaily() throws Exception {
+
+    }
+}
