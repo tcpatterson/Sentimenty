16a0a5b5c84411a481b248d41dd39f59eb39f599
Neel Kuila <neelk1123@gmail.com>
2016-03-21 21:48:46 -0400

    Trends display. Again

--- src/main/resources/static/js/trends.js
+++ src/main/resources/static/js/trends.js
@@ -1,36 +1,35 @@
 $( document ).ready(function() {
-    var allArticles;
-    $.get( "/showPress", function( data ) {
-      data.forEach(function(d) {
-        var jsonn = JSON.parse(d.sentiment);
-        d.sentiment = jsonn;
-        if(d.sentiment && d.sentiment.score){
-            d.sentiment.score = d.sentiment.score.substring(0,6);
-        } else {
-            d.sentiment = {
-                score: 1
-            };
-            d.sentiment.score = "0.00";
+//    $.get( "/showPress", function( data ) {
+//      data.forEach(function(d) {
+//        var jsonn = JSON.parse(d.sentiment);
+//        d.sentiment = jsonn;
+//        if(d.sentiment && d.sentiment.score){
+//            d.sentiment.score = d.sentiment.score.substring(0,6);
+//        } else {
+//            d.sentiment = {
+//                score: 1
+//            };
+//            d.sentiment.score = "0.00";
+//        }
+//        if(d.sentiment.score < 0 ) {
+//            d.arrow = "bottom";
+//            d.color = "red";
+//        } else {
+//            d.arrow = "top";
+//            d.color = "green";
+//        }
+//        var date = new Date(d.time);
+//        d.time = date;
+//      });
+//      $("#clientTemplate").tmpl(data).appendTo( "#mentions" );
+      var NUM_OF_TRENDS_TO_DISPLAY = 4
+
+      $.get("/trends", function ( trendMentionList ) {
+        for (var i = 0; i < NUM_OF_TRENDS_TO_DISPLAY; i++){
+            $("<button>"+trendMentionList[0][i]+"</button>").addClass("trendButton").appendTo(".trend")
         }
-        if(d.sentiment.score < 0 ) {
-            d.arrow = "bottom";
-            d.color = "red";
-        } else {
-            d.arrow = "top";
-            d.color = "green";
-        }
-        var date = new Date(d.time);
-        d.time = date;
-      });
-      $("#clientTemplate").tmpl(data).appendTo( "#mentions" );
-
-      $.get("/trend", function ( trendMentionList ) {
-                  trendMentionList[0]//This is the list of trend titles
-                  trendMentionList[1]//This is the list of article mentions
-
-      })
-    });
-
+        trendMentionList[1]//This is the list of article mentions
+  })
 });
 
 $("#reuters").click(function() {
