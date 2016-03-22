1b1bedbd2e885ac9f7625ca3795dd22b6746595b
Neel Kuila <neelk1123@gmail.com>
2016-03-21 23:20:47 -0400

    Attempting to display trends

--- src/main/resources/static/js/trends.js
+++ src/main/resources/static/js/trends.js
@@ -22,13 +22,44 @@ $( document ).ready(function() {
 //        d.time = date;
 //      });
 //      $("#clientTemplate").tmpl(data).appendTo( "#mentions" );
-      var NUM_OF_TRENDS_TO_DISPLAY = 4
+  var NUM_OF_TRENDS_TO_DISPLAY = 4
 
-      $.get("/trends", function ( trendMentionList ) {
-        for (var i = 0; i < NUM_OF_TRENDS_TO_DISPLAY; i++){
-            $("<button>"+trendMentionList[0][i]+"</button>").addClass("trendButton").appendTo(".trend")
+  $.get("/trends", function ( trendMentionList ) {
+    for (var i = 0; i < NUM_OF_TRENDS_TO_DISPLAY; i++){
+        $("<button>"+trendMentionList[0][i]+"</button>").addClass("trendButton").addClass(""+i).appendTo(".trend")
+    }
+
+    var showList = trendMentionList[1];
+    for (var i = 0; i < trendMentionList[1]; i++ ){//List of List of Press
+        for (var j = 0; j < trendMentionList[1][i]; j++){//List of Press
+            var apiArticle = trendMentionList[1][i][j]
+            frontendArticle = apiArticle
+            var parsedSentiment = JSON.parse(frontendArticle.sentiment)
+            frontendArticle.sentiment = parsedSentiment
+            if ( frontendArticle.sentiment && frontendArticle.sentiment.score ){
+                var sentScore = parseInt(frontendArticle.sentiment.score)
+                sentScore = 50*sentScore+50
+                frontendArticle.sentiment.score = "" + sentScore
+            } else {
+                frontendArticle.sentiment = {
+                    score: 1
+                };
+                frontendArticle.sentiment.score = "0.00"
+            }
+            if (frontendArticle.sentiment.score < 0) {
+                frontendArticle.arrow = "bottom"
+                frontendArticle.color = "red"
+            } else {
+                frontendArticle.arrow = "top"
+                frontendArticle.color = "green"
+            }
+            var date = new Date(frontendArticle.time)
+            frontendArticle.time = date
+            showList[i][j] = frontendArticle
         }
-        trendMentionList[1]//This is the list of article mentions
+    }
+
+    $('#clientTemplate').tmpl(showList).appendTo("#mentions")
   })
 });
 
