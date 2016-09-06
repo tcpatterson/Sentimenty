c97229ef14c8d126717965b6b9b30f78441d175b
Connor Patterson <tcpatter@ncsu.edu>
2016-09-06 00:40:47 -0400

    port binding

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 docker build -t tcpatter/sentimenty ./
 wd=$(pwd)
-docker run -v "${wd}/app":/home/sentimenty -p 8080:8080 --name sentimenty -d tcpatter/sentimenty
+docker run -v "${wd}/app":/home/sentimenty -p 6060:8080 --name sentimenty -d tcpatter/sentimenty
